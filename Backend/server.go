d57a079176d6d4fd0d1a41e2e1735da32d7e12e4
Isaac <lee3650@purdue.edu>
2024-02-02 21:52:05 -0500

    Read request parameters

--- Backend/server.go
+++ Backend/server.go
@@ -5,10 +5,22 @@ import (
 	"fmt"
 )
 
+func set(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, fmt.Sprintf("state: %s", r.FormValue("state"))) 
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, fmt.Sprintf("device id: %s", r.FormValue("deviceId")))
+}
+
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
     })
+
+    http.HandleFunc("/set", set)
+
+    http.HandleFunc("/get", get)
 
     http.ListenAndServe(":80", nil)
 }
